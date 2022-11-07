c6e6ae2394304cf659169a118c8336e5aa025e73
bailliem <bailliem@gmail.com>
2022-11-07 21:16:55 +0100

    add plots to sc

---
+++ missing-data-plot.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(naniar)
+
+# Read in data ---------------------
+nov_data <- read_csv("nov-data.csv")
+
+
+
+glimpse(nov_data)
+
+
+nov_data %>% 
+  select(starts_with("WEEK")) %>%
+  vis_miss()
+
+
+nov_data %>% 
+  select(starts_with("WEEK")) %>%
+  gg_miss_upset(nsets = n_var_miss(nov_data))
+
+
+
+ggplot(nov_data,
+       aes(x = WEEK27,
+           y = WEEK48)) +
+  geom_miss_point() + 
+  facet_grid(BMIGR ~ AGEGR)
+
+
+ggplot(nov_data,
+       aes(x = WEEK27,
+           y = WEEK48)) +
+  geom_miss_point() + 
+  facet_grid(AGEGR ~ ARM )
+
+
+
+gg_miss_var(nov_data)
+
+
+gg_miss_var(nov_data,
+            facet = ARM, show_pct = TRUE)
+
+
+
+
+gg_miss_case(nov_data, order_cases = TRUE, facet = ARM)
+
+
+gg_miss_fct(x = nov_data, fct = ARM)
+
